7b191a5e22fabdde6543d79acaf0fe2df35604f3
Sardorbek3101 <oscodeer@gmail.com>
2025-06-11 04:33:03 +0500

    ddd

--- scriptj.js
+++ scriptj.js
@@ -1,9 +1,17 @@
 (() => {
   let lastRightClick = 0;
-  const RAPIDAPI_KEY = "e46117ae21msh918b1b8b54d4e47p1c1623jsnbfc839744a88";
-  document.addEventListener("contextmenu", e => e.stopImmediatePropagation(), true);
-  document.addEventListener("mousedown", e => e.stopImmediatePropagation(), true);
-  document.addEventListener("mouseup", e => e.stopImmediatePropagation(), true);
+  document.addEventListener("contextmenu", e => {
+    e.preventDefault();               // Отключает стандартное меню
+    e.stopImmediatePropagation();     // Блокирует перехват события другими скриптами
+  }, true);
+
+  document.addEventListener("mousedown", e => {
+    e.stopImmediatePropagation();     // Блокируем попытки сайта перехватить клик
+  }, true);
+
+  document.addEventListener("mouseup", e => {
+    e.stopImmediatePropagation();     // Блокируем попытки сайта перехватить клик
+  }, true);
 
   // Показываем "success" при движении мыши
   document.addEventListener("mousemove", function showSuccessOnce(e) {
